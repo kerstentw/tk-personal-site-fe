0cb07747739db6b4e81f1249bcd3fc68e12542e9
kerstentw <twkersten@gmail.com>
2023-04-05 23:42:56 +0800

    Finished CV Page

--- src/components/frames/CV.js
+++ src/components/frames/CV.js
@@ -1,9 +1,22 @@
+import "../css/CV.css";
 import PageHeader from "../basics/PageHeader";
+import resumePDF from "../../static/TK_Resume.pdf";
+import cvDownloadImg from "../../static/cv_download.svg";
+import {Container} from "react-bootstrap";
 
 export default function CV () {
     return (
         <div>
           <PageHeader headingText=".curriculum-vitae"/>
+          <Container className="cv-content-wrapper">
+            <a target="_blank" href={resumePDF}>
+                <img className="cv-download-image" src={cvDownloadImg}/>
+            </a>
+
+            <p className="cv-directions">
+                  Best viewed on a Desktop with a mouse. Click if you are just browsing.  Hover if you wanna talk.  
+            </p>
+          </Container>
         </div>
     )
 }
